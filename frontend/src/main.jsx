41c8c1ab77897f66f980d1a689f1e43bfaf0fe24
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2025-12-03 11:17:41 -0300

    refactor(main): change hashRouter to BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router';
+import { BrowserRouter } from 'react-router';
 
 import App from './components/App.jsx';
 
@@ -8,8 +8,8 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 );
