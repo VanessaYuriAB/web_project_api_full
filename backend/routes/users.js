7afa8e043532a01ba29aa452cf893080136a5a93
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2025-12-09 11:43:35 -0300

    feat(celebrate): implement validations for '/users' routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,12 +8,20 @@ const {
   getUser,
 } = require('../controllers/users');
 
+const celebrateForUserId = require('../middlewares/validators/users/celebrateForUserId');
+const celebrateForUserUpdate = require('../middlewares/validators/users/celebrateForUserUpdate');
+const celebrateForAvatarUpdate = require('../middlewares/validators/users/celebrateForAvatarUpdate');
+
 // Rotas para usuários ('/users')
+
+// Sem validação do celebrate pq não possuem params nem body
 usersRouter.get('/', getUsers);
 usersRouter.get('/me', getUser); // declarada antes de '/:id' para 'me' não ser interpretado como req.params e dar erro de Cast
-usersRouter.get('/:userId', getUserById);
-usersRouter.patch('/me', updateUser);
-usersRouter.patch('/me/avatar', updateAvatar);
+
+// Com validação do celebrate
+usersRouter.get('/:userId', celebrateForUserId, getUserById);
+usersRouter.patch('/me', celebrateForUserUpdate, updateUser);
+usersRouter.patch('/me/avatar', celebrateForAvatarUpdate, updateAvatar);
 
 // Exporta o roteador de usuários
 module.exports = usersRouter;
