9e3f9813f9e8c1048ed28bafad38d356a094d9e5
Vanessa Yuri A. Brito <emaildavanessayuri@gmail.com>
2025-12-09 11:45:22 -0300

    feat(celebrate): add validation middleware for newCard

---
+++ backend/middlewares/validators/cards/celebrateForNewCard.js
@@ -0,0 +1,11 @@
+const { celebrate, Joi } = require('celebrate');
+const validateURL = require('../validateURL');
+
+const celebrateForNewCard = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().custom(validateURL),
+  }),
+});
+
+module.exports = celebrateForNewCard;
